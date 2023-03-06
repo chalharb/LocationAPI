09283e69c9c744a7b84309d7fdcd98e7f4b8b980
Logan Harber <chalharb@gmail.com>
2023-03-05 21:56:27 -0600

    feat: Created and stubbed out Location Repository

---
+++ Contracts/ILocationRepository.cs
@@ -0,0 +1,22 @@
+﻿using LocationAPI.Models;
+namespace LocationAPI.Services
+{
+    public interface ILocationRepository
+    {
+        // Todo: Separate these into individual repositories that inherit from a base repository
+        Task<IEnumerable<State>> GetAllStatesAsync();
+        Task<State?> GetStateByIdAsync(Guid id);
+        void CreateState(State state);
+        void UpdateState(State state);
+        void DeleteState(State state);
+        Task<bool> DoesStateExist(Guid id);
+        Task<IEnumerable<State>> GetAllCitiesAsync();
+        Task<IEnumerable<State>> GetAllCitiesByStateAsync(Guid stateId);
+        Task<State?> GetCityByIdAsync(Guid id);
+        void CreateCity(City city);
+        void UpdateCity(City city);
+        void DeleteCity(Guid id);
+        Task<bool> DoesCityExist(Guid id);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ Contracts/LocationRepository.cs
@@ -0,0 +1,86 @@
+﻿using LocationAPI.DbContexts;
+using LocationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocationAPI.Services
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly LocationDbContext _dbContext;
+
+        public LocationRepository(LocationDbContext context)
+        {
+            _dbContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void CreateCity(City city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateState(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCity(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteState(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesCityExist(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesStateExist(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<State>> GetAllCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<State>> GetAllCitiesByStateAsync(Guid stateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<State>> GetAllStatesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<State?> GetCityByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<State?> GetStateByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCity(City city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateState(State state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
